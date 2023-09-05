83e8681eab88d656b6acfba9b120d02754a330a1
RichardCBravo <richardkbravo@gmail.com>
2023-09-05 08:17:15 -0500

    Inicio de proyecto con Spring Initializr

---
+++ src/main/java/com/nexos/inventory/InventoryNexosApplication.java
@@ -0,0 +1,13 @@
+package com.nexos.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryNexosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryNexosApplication.class, args);
+	}
+
+}
